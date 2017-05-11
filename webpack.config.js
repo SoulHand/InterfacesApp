f829dd17cb9c768b2b40823274f8c9fe5124ecbd
Jhonny Mata <solucionesmatajm@gmail.com>
2017-05-11 15:17:30 -0400

    innecesario

--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,61 @@
 var webpack = require('webpack');
 
-// webpack.config.js 
 module.exports = {
-    entry: './src/js/main.js',
-    output: {
-        path: './dist',
-        filename: 'app.js'
-    },
-    module: {
-        loaders: [
-          { test: /\.scss$/, loader: "style-loader!css-loader!sass-loader" },
-          { test: /\.css$/, loader: "style-loader!css-loader" }
-        ],
-    },
-    plugins: [
+  entry: {
+    app:'./src/js/main.js',
+    style:'./src/css/index.scss'
+  },
+  output: {
+    path: __dirname + '/dist/',
+    filename: '[name].js'
+  },
+  resolve: {
+    extensions: ['.js', '.webpack.js', '.web.js'],
+    modules: ['node_modules']
+  },
+  // devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: "url-loader?mimetype=application/font-woff"
+      },
+      {
+        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: "file-loader?name=[name].[ext]"
+      },
+      {
+        test: /\.(gif|png|jpeg|jpg)$/,
+        loader: "file-loader?name=[name].[ext]"
+      },
+      {
+        test: /\.(scss|css)$/,
+        use: ['style-loader',"css-loader","sass-loader"]
+        //loader: "style-loader!css-loader!sass-loader",
+        //options: { importLoaders: 1 }
+      }
+    ]
+  },
+  /*
+  externals: {
+    "react": "React",
+    "react-dom": "ReactDOM",
+    "react-router": "ReactRouter"
+  },*/
+  plugins: [
     new webpack.ProvidePlugin({
        $: "jquery",
        jQuery: "jquery",
-       Tether:"tether",
-       React: "react",
-       ReactDOM: "react-dom",
-       ReactRouter: "react-router"
+       Tether:"tether"
     }),
     // new webpack.optimize.CommonsChunkPlugin('vendor', 'vendor.bundle.js'),
-    new webpack.optimize.UglifyJsPlugin({
+    /*new webpack.optimize.UglifyJsPlugin({
         compress: {
             warnings: false
         }
-    }),
+    }),*/
     new webpack.DefinePlugin({
         NODE_ENV: 'development'
-    })/*,
-     new BowerWebpackPlugin({
-      modulesDirectories: ["bower_components"],
-      manifestFiles:      "bower.json",
-      includes:           "/.*\/",
-      excludes:           [],
-      searchResolveModulesDirectories: true
-    })*/   
+    })
   ]
-};
+}
